48adae7a809fa29b8671a76c074ce8c320e071ba
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-05-11 15:11:22 -0400

    delete an item from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,8 @@
 class CartsController < ApplicationController
+
+  def clean
+    current_cart.clean!
+    flash[:warning] = "You cart is empty now"
+    redirect_to carts_path    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,26 @@ Rails.application.routes.draw do
 
   root "products#index"
 
+  # products
   resources :products do
     member do
       post :add_to_cart
     end
   end
 
+  # admin
   namespace :admin do
     resources :products
   end
 
-  resources :carts 
+  # carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 
+  #cart_items    
+  resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
