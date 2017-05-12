c3b3fad31269f747b16ecc4448f958fd670037cc
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-05-12 17:12:11 -0400

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # admin
   namespace :admin do
     resources :products
+    resources :orders   
   end
 
   namespace :account do
@@ -35,7 +36,7 @@ Rails.application.routes.draw do
   resources :orders do
     member do
       post :pay_with_alipay
-      post :pay_with_wechat  
+      post :pay_with_wechat
     end
   end
 
