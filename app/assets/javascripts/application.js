d6b75810d57d7c9a8548b122ba458c9ed7f38418
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-05-30 11:45:29 -0400

    fix product index css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,13 +17,12 @@
 //= require bootstrap/dropdown
 //= require bootstrap-sprockets
 //= require bootstrap/carousel
-//= require_tree .
 //= require bootstrap/modal
+//= require_tree .
 
 $(document).ready(function(){
   // Activate carousel
   $("#myCarousel").carousel({interval: 5000});
-
   // Enable carousel Indicators
   $(".item1").click(function(){
     $("#myCarousel").carousel(0);
@@ -34,58 +33,11 @@ $(document).ready(function(){
   $(".item3").click(function(){
     $("#myCarousel").carousel(2);
   });
-
   // Enable Carousel Controls
   $(".left").click(function(){
     $("#myCarousel").carousel("prev");
   });
   $(".right").click(function(){
     $("#myCarousel").carousel("next");
-  });
-
-});
-
-
-
-//-------------- product index --------------
-$(".box i").click(function() {
-
-  var productImg = $(this).siblings("img").attr('src');
-  var productimgNo = $(this).siblings("img").attr('alt');
-  var productName = $(this).siblings("h3").text();
-  var productPrice = $(this).siblings("p").text();
-  var productQty = 1;
-  var total = parseFloat(productPrice);
-  var info = $('<div class="row">' + '<img src="' + productImg + '" class="' + productimgNo + '">' + '<h4>' + productName + "</h4>" + '<p>' + productPrice + "</p>" + '<div class="qty-minus">-</div>' + '<div class="qty">' + productQty + '</div>' + '<div class="qty-plus">+</div>' + '<div class="del">Remove</div>' + '<div class="totalprice">' + total + '</div>' + '</div>');
-
-  /*add item*/
-  $("#cart").append(info);
-
-  /*if item exist, add quantity*/
-  if ($('.pic1').size() > 1) {
-    var newQty = parseInt($('.pic1').first().siblings('.qty').text()) + 1;
-    $('.pic1').parents('.row').first().remove();
-    $('.pic1').siblings('.qty').text(newQty);
-    total = newQty*productPrice;
-    $('.pic1').siblings('.totalprice').text(total);
-  } else if ($('.row .pic2').size() > 1) {
-    var newQty = parseInt($('.pic2').first().siblings('.qty').text()) + 1;
-    $('.pic2').parents('.row').first().remove();
-    $('.pic2').siblings('.qty').text(newQty);
-    total = newQty*productPrice;
-    $('.pic2').siblings('.totalprice').text(total);
-  } else if ($('.pic3').size() > 1) {
-    var newQty = parseInt($('.pic3').first().siblings('.qty').text()) + 1;
-    $('.pic3').parents('.row').first().remove();
-    $('.pic3').siblings('.qty').text(newQty);
-    total = newQty*productPrice;
-    $('.pic3').siblings('.totalprice').text(total);
-  }
-
-
-
-  /*delete item*/
-  $('.del').click(function() {
-    $(this).parents(".row").remove();
   });
 });
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,11 +24,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-   process resize_to_fill: [250,250] 
+   process resize_to_fill: [200,200]
   end
 
   version :medium do
-   process resize_to_fill: [400,400]
+   process resize_to_fill: [500,550]    
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
